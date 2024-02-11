1930eafbfe8cb856b0c0e1302de6e1a26a91311b
avan78 <145610657+avan78@users.noreply.github.com>
2024-02-11 23:45:02 +0100

    Routing - basics implementing

--- artist/src/App.js
+++ artist/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import './App.css';
 import {MainPage} from "./Pages/MainPage";
+import {ErrorPage} from "./Pages/ErrorPage";
 
 function App() {
+    const router = createBrowserRouter([{
+        path:"/",
+        element:<MainPage/>,
+        errorElement:<ErrorPage/>,
+    }])
   return (
     <div className="App">
+        <RouterProvider router={router}>
         <MainPage/>
+        </RouterProvider>
     </div>
   );
 }
